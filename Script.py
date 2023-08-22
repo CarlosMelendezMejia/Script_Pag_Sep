e925f9f2c15ea22f57d35cdc647948c2683ab7ee
CarlosMelendezMejia <72164372+CarlosMelendezMejia@users.noreply.github.com>
2023-08-22 14:17:07 -0600

    Comentarios y correcciones

--- Script.py
+++ Script.py
@@ -3,67 +3,138 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-# tipos de libros
-tag_books = ('LMP','MLA','PAA','PCA','PEA','SDA','TPA','CMA','SHA')
+# Constantes
+TAG_BOOKS = ('LMP', 'MLA', 'PAA', 'PCA', 'PEA', 'SDA', 'TPA', 'CMA', 'SHA')
+BASE_URL = 'https://www.conaliteg.sep.gob.mx/2023/c/'
+BASE2_URL = 'https://www.conaliteg.sep.gob.mx/2023/'
+MAX_IMAGES_PER_BOOK = 400
 
-#URL bases para la descarga de las imagenes y la busqueda del libro
-base_url = 'https://www.conaliteg.sep.gob.mx/2023/c/'
-base2_url = 'https://www.conaliteg.sep.gob.mx/2023/'
+def download_images(book_url, output_folder):
+    # Crea la carpeta de salida si no existe
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-#Contador para P
-for i in range (0,8):
-    #Contador para los Titulos de los libros    
-    for le in range (0,len(tag_books)):
-        #Try y cach por si no encuentra la pagina
-        try:
-            # URL de los libros
-            url =f'{base_url}P{i}{tag_books[le]}/'
-            #url para buscar si existe el libro
-            urlbusqueda =f'{base2_url}P{i}{tag_books[le]}.htm#page/2'
-                        
-            # Realizar la solicitud HTTP a la página web
-            response = requests.get(urlbusqueda)
-            response.raise_for_status()
+    base = "000"
+    # Itera a través del rango de imágenes por libro
+    for img_cont in range(1, MAX_IMAGES_PER_BOOK + 1):
+        img_num = f"{base}{img_cont}"[-len(base):]
+        img_url = f'{book_url}{img_num}.jpg'
+        img_name = img_url.split('/')[-1]
+        img_path = os.path.join(output_folder, img_name)
 
-            # Analizar el contenido HTML utilizando BeautifulSoup
-            soup = BeautifulSoup(response.content, 'html.parser')
+        # Realiza la solicitud HTTP para descargar la imagen
+        response = requests.get(img_url)
+        if response.status_code == 200:
+            # Guarda la imagen en el archivo local
+            with open(img_path, 'wb') as img_file:
+                img_file.write(response.content)
+            print(f'Imagen descargada: {img_name}')
+        else:
+            # Si no se puede descargar la imagen, sale del bucle
+            break
 
-            #INTENTA ENCONTRAR LAS IMAGENES, DA OTRA COSA 
-            img_tags = soup.find_all('img')
+def main():
+    # Itera a través de los índices de P y los tipos de libros
+    for i in range(8):
+        for tag in TAG_BOOKS:
+            try:
+                # Construye las URL para buscar libros y descargar imágenes
+                book_url = f'{BASE_URL}P{i}{tag}/'
+                book_search_url = f'{BASE2_URL}P{i}{tag}.htm#page/2'
 
-            #Donde se va a guardar
-            output_folder = f'P{i}{tag_books[le]}'
-            if not os.path.exists(output_folder):
-                os.makedirs(output_folder)
+                # Realiza la solicitud HTTP para buscar libros
+                response = requests.get(book_search_url)
+                response.raise_for_status()
 
-            absolute_path = os.path.abspath(output_folder)
+                # Analiza el contenido HTML de la página
+                soup = BeautifulSoup(response.content, 'html.parser')
+                img_tags = soup.find_all('img')
 
-            #Base para el numero de pagina
-            base = "000"  
+                # Define la carpeta de salida y la ruta absoluta
+                output_folder = f'P{i}{tag}'
+                absolute_path = os.path.abspath(output_folder)
 
-            # Descargar las imágenes
-            for img_cont in range (1,400):
-                img_num = f"{base}{img_cont}"[-len(base):]  # Asegura que solo los últimos caracteres se conserven
-                # Obtener la URL de la imagen
-                img_url =f'{url}{img_num}.jpg'
-                img_name = img_url.split('/')[-1]  # Obtener el nombre de archivo de la URL
-                img_path = os.path.join(output_folder, img_name)  # Ruta completa de destino
-                
-                
-                response = requests.get(img_url)  # Realizar la solicitud HTTP
+                # Descarga las imágenes del libro
+                download_images(book_url, output_folder)
 
+                # Imprime mensajes al completar la descarga
+                print('Descarga completada.')
+                print(f'Ruta absoluta de la carpeta de imágenes descargadas: {absolute_path}')
 
-                if response.status_code == 200:
-                    with open(img_path, 'wb') as img_file:
-                        img_file.write(response.content)
-                else:
-                    break
-                
-                print(f'Imagen descargada: {img_name}')
+            except requests.exceptions.RequestException as e:
+                # Maneja la excepción si no se encuentra el libro
+                print(f'No existe el libro {book_url}: {e}')
+                continue
 
-        except requests.exceptions.RequestException as e:
-            print(f'No existe el libro {url}: {e}')
-            continue
+if __name__ == "__main__":
+    main()
+import os
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-        print('Descarga completada.')
-        print(f'Ruta absoluta de la carpeta de imágenes descargadas: {absolute_path}')
+# Constantes
+TAG_BOOKS = ('LMP', 'MLA', 'PAA', 'PCA', 'PEA', 'SDA', 'TPA', 'CMA', 'SHA')
+BASE_URL = 'https://www.conaliteg.sep.gob.mx/2023/c/'
+BASE2_URL = 'https://www.conaliteg.sep.gob.mx/2023/'
+MAX_IMAGES_PER_BOOK = 400
+
+def download_images(book_url, output_folder):
+    # Crea la carpeta de salida si no existe
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    base = "000"
+    # Itera a través del rango de imágenes por libro
+    for img_cont in range(1, MAX_IMAGES_PER_BOOK + 1):
+        img_num = f"{base}{img_cont}"[-len(base):]
+        img_url = f'{book_url}{img_num}.jpg'
+        img_name = img_url.split('/')[-1]
+        img_path = os.path.join(output_folder, img_name)
+
+        # Realiza la solicitud HTTP para descargar la imagen
+        response = requests.get(img_url)
+        if response.status_code == 200:
+            # Guarda la imagen en el archivo local
+            with open(img_path, 'wb') as img_file:
+                img_file.write(response.content)
+            print(f'Imagen descargada: {img_name}')
+        else:
+            # Si no se puede descargar la imagen, sale del bucle
+            break
+
+def main():
+    # Itera a través de los índices de P y los tipos de libros
+    for i in range(8):
+        for tag in TAG_BOOKS:
+            try:
+                # Construye las URL para buscar libros y descargar imágenes
+                book_url = f'{BASE_URL}P{i}{tag}/'
+                book_search_url = f'{BASE2_URL}P{i}{tag}.htm#page/2'
+
+                # Realiza la solicitud HTTP para buscar libros
+                response = requests.get(book_search_url)
+                response.raise_for_status()
+
+                # Analiza el contenido HTML de la página
+                soup = BeautifulSoup(response.content, 'html.parser')
+                img_tags = soup.find_all('img')
+
+                # Define la carpeta de salida y la ruta absoluta
+                output_folder = f'P{i}{tag}'
+                absolute_path = os.path.abspath(output_folder)
+
+                # Descarga las imágenes del libro
+                download_images(book_url, output_folder)
+
+                # Imprime mensajes al completar la descarga
+                print('Descarga completada.')
+                print(f'Ruta absoluta de la carpeta de imágenes descargadas: {absolute_path}')
+
+            except requests.exceptions.RequestException as e:
+                # Maneja la excepción si no se encuentra el libro
+                print(f'No existe el libro {book_url}: {e}')
+                continue
+
+if __name__ == "__main__":
+    main()
